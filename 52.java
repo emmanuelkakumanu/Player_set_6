7701b6832dce3b9d9fc8b2c304fa17f532f3acd4
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-08-24 11:33:42 +0530

    print Kth smallest number in an array

---
+++ 52.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int k=s.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		a[i]=s.nextInt();
+		if(n<=0 || n<k)
+		System.out.print("Invalid");
+		else 
+		{
+			for(int i=0;i<n;i++)
+			{
+				for(int j=i+1;j<n;j++)
+				{
+					if(a[i]>a[j])
+					{
+						int temp=a[i];
+						a[i]=a[j];	
+						a[j]=temp;
+					}
+				}
+			}
+			System.out.print(a[k-1]);
+		}
+	}
+}
