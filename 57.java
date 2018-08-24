04576b3f0bb4a3c4b94a3fd7dcdcd02148350062
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-08-24 12:05:30 +0530

    print the number of occurrences of ch in str

---
+++ 57.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		String str=s.next();
+		char ch=s.next().charAt(0);int f=0;
+		for(int i=0;i<str.length();i++)
+		{
+			if(str.charAt(i)==ch)
+			{
+				f++;
+			}
+		}
+		System.out.print(f);
+	}
+}
